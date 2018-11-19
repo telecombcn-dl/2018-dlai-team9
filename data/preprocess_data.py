af682f4b0031134c63ae710f1080f093578723b4
clarabonnin <clabonros@gmail.com>
2018-11-19 17:37:45 +0100

    Playing with shape of input

--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -15,8 +15,14 @@ def mapped_batch(image_batch):
     :type image_batch: list
     :return: inputs, labels
     """
+    image_size = image_batch[0].shape
     inputs, labels = list(), list()
-    for image in image_batch:
-        inputs.append(np.expand_dims(image[:, :, 0],axis =2))
+    for i, image in enumerate(image_batch):
+        input = np.zeros((1, len(image_batch), image_size[0], image_size[0]))
+        input[0, i, :, :] = image[:, :, 0]
         labels.append(inverse_h(image[:, :, 1:]))
     return inputs, labels
+
+
+
+print('hi')
