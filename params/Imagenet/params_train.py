513b87b90ed278576b92afe1f2440012f73e99fd
clarabonnin <clabonros@gmail.com>
2018-12-01 13:15:41 +0100

    Solving errors in loss function

--- params/Imagenet/params_train.py
+++ params/Imagenet/params_train.py
@@ -5,7 +5,7 @@ def get_params():
     params = {
         p.INPUT_SHAPE: [256, 256],  # Imagenet size
         p.N_EPOCHS: 50,
-        p.BATCH_SIZE: 100,
+        p.BATCH_SIZE: 10,
         p.LR: 0.0005,  # Learning rate
 
         p.N_IMAGES_TRAIN_VAL: 1000,
