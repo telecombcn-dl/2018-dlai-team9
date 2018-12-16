c553884f3d2bdd94af343f601e942772fee9bb5b
clarabonnin <clabonros@gmail.com>
2018-12-16 10:33:00 +0100

    gan architecture

--- model/cnn_net.py
+++ model/cnn_net.py
@@ -1,4 +1,7 @@
 # This file contains the CNN definition
+import tensorflow as tf
+import numpy as np
+from keras import backend as K
 from keras.optimizers import Adam
 from keras.regularizers import l1_l2
 from keras.models import Model
@@ -6,8 +9,11 @@ from keras.losses import categorical_crossentropy
 from model.loss import categorical_crossentropy_weighted_function
 from keras.activations import softmax
 from keras.metrics import mse
+from keras import objectives
 from keras.layers import Input, Activation, ZeroPadding2D, BatchNormalization, Conv2D, Deconv2D, MaxPooling2D, \
-    Concatenate, Add, UpSampling2D
+    Concatenate, Add, UpSampling2D, LeakyReLU, GlobalAveragePooling2D, Dense, Lambda
+
+
 
 
 def graph(input_shape):
@@ -325,6 +331,120 @@ def unet(input_shape=(256, 256, 1), l1=0.00001, l2=0.005):
     model = Model(inputs=x, outputs=y)
 
     return model
+
+
+def discriminator(patch_height=256, patch_width=256, channels=313):
+    """
+    It creates the discriminator, compiles and return the model
+    :param patch_height: height of the input images
+    :param patch_width: width of the input images
+    :param channels: q = 313
+    :return: the discriminator
+    """
+
+    k = 3  # kernel size
+    s = 2  # stride
+    n_filters = 32  # number of filters
+
+    inputs = Input((patch_height, patch_width, channels+1))
+    conv1 = Conv2D(n_filters, kernel_size=(k, k), strides=(s, s), padding='same')(inputs)
+    conv1 = BatchNormalization(scale=False, axis=3)(conv1)
+    conv1 = LeakyReLU(alpha=0.1)(conv1)
+    conv1 = Conv2D(n_filters, kernel_size=(k, k), padding='same')(conv1)
+    conv1 = BatchNormalization(scale=False, axis=3)(conv1)
+    conv1 = LeakyReLU(alpha=0.1)(conv1)
+    pool1 = MaxPooling2D(pool_size=(s, s))(conv1)
+
+    conv2 = Conv2D(2 * n_filters, kernel_size=(k, k), strides=(s, s), padding='same')(pool1)
+    conv2 = BatchNormalization(scale=False, axis=3)(conv2)
+    conv2 = LeakyReLU(alpha=0.1)(conv2)
+    conv2 = Conv2D(2 * n_filters, kernel_size=(k, k), padding='same')(conv2)
+    conv2 = BatchNormalization(scale=False, axis=3)(conv2)
+    conv2 = LeakyReLU(alpha=0.1)(conv2)
+    pool2 = MaxPooling2D(pool_size=(s, s))(conv2)
+
+    conv3 = Conv2D(4 * n_filters, kernel_size=(k, k), padding='same')(pool2)
+    conv3 = BatchNormalization(scale=False, axis=3)(conv3)
+    conv3 = LeakyReLU(alpha=0.1)(conv3)
+    conv3 = Conv2D(4 * n_filters, kernel_size=(k, k), padding='same')(conv3)
+    conv3 = BatchNormalization(scale=False, axis=3)(conv3)
+    conv3 = LeakyReLU(alpha=0.1)(conv3)
+    pool3 = MaxPooling2D(pool_size=(s, s))(conv3)
+
+    conv4 = Conv2D(8 * n_filters, kernel_size=(k, k), padding='same')(pool3)
+    conv4 = BatchNormalization(scale=False, axis=3)(conv4)
+    conv4 = LeakyReLU(alpha=0.1)(conv4)
+    conv4 = Conv2D(8 * n_filters, kernel_size=(k, k), padding='same')(conv4)
+    conv4 = BatchNormalization(scale=False, axis=3)(conv4)
+    conv4 = LeakyReLU(alpha=0.1)(conv4)
+    pool4 = MaxPooling2D(pool_size=(s, s))(conv4)
+
+    conv5 = Conv2D(16 * n_filters, kernel_size=(k, k), padding='same')(pool4)
+    conv5 = BatchNormalization(scale=False, axis=3)(conv5)
+    conv5 = LeakyReLU(alpha=0.1)(conv5)
+    conv5 = Conv2D(16 * n_filters, kernel_size=(k, k), padding='same')(conv5)
+    conv5 = BatchNormalization(scale=False, axis=3)(conv5)
+    conv5 = LeakyReLU(alpha=0.1)(conv5)
+
+    gap = GlobalAveragePooling2D()(conv5)
+    outputs = Dense(1, activation='sigmoid')(gap)
+
+    d = Model(inputs, outputs)
+
+    # loss of the discriminator. it is a binary loss
+    def d_loss(y_true, y_pred):
+        #L = objectives.binary_crossentropy(K.batch_flatten(y_true), K.batch_flatten(y_pred))
+        L = objectives.mean_squared_error(K.batch_flatten(y_true), K.batch_flatten(y_pred))
+        return L
+
+    d.compile(optimizer=Adam(lr=1e-4), loss=d_loss, metrics=['accuracy'])
+
+    return d
+
+
+def get_gan(g, d, prior_probs):
+    """
+    GAN (that binds generator and discriminator)
+    It gets the combined and compiles model (graph + discriminator)
+    :param g: colorization model
+    :param d: discriminative model
+    :return: the combined model (graph + discriminator)
+    """
+
+    def resize_labels(fake_labels):
+        fake_labels = tf.convert_to_tensor(np.array(fake_labels))
+        fake_labels = tf.reshape(fake_labels, (None, 64, 64, 313))
+        return tf.image.resize_images(fake_labels, [256, 256])
+
+    image = Input((256, 256, 1))
+    labels = Input((64, 64, 313))
+
+    fake_labels = g(image)
+    upsampled_fake_labels = Lambda(resize_labels)([fake_labels])
+
+    fake_pair = Concatenate(axis=3)([image, upsampled_fake_labels])
+
+    gan = Model([image, labels], d(fake_pair))
+
+
+    def gan_loss(y_true, y_pred):
+
+        # trade-off coefficient
+        alpha_recip = 0.05
+
+        y_true_flat = K.batch_flatten(y_true)
+        y_pred_flat = K.batch_flatten(y_pred)
+
+        L_adv = objectives.mean_squared_error(y_true_flat, y_pred_flat)
+
+        categorical_crossentropy_weighted = categorical_crossentropy_weighted_function(prior_probs)
+        L_col = categorical_crossentropy_weighted(labels, fake_labels)
+
+        return alpha_recip * L_adv + L_col
+
+    gan.compile(optimizer=Adam(lr=1e-4), loss=gan_loss, metrics=['accuracy'])
+
+    return gan
 
 
 def compile_model(model, lr=0.005, optimizer_name='Adam', loss_name='cross_entropy_weighted', prior_probs=None):
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-srun --gres=gpu:1,gmem:10GB --pty --mem=10G python train.py
+srun --gres=gpu:1,gmem:10GB --pty --mem=10G python -W ignore train.py
