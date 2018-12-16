54500700142d7962800855bc7f36cca0b3a0ddbf
JMarcoRimmek <joanmarco96@gmail.com>
2018-12-16 19:06:57 +0100

    Joan: Added custom loss function when loading model in eval. Minor changes in prints too.

--- train.py
+++ train.py
@@ -9,16 +9,16 @@ from model.cnn_net import graph, compile_model
 class Trainer(object):
     def __init__(self):
         # Parameters
-        self.LR = 0.005
+        self.LR = 0.0005
         self.N_EPOCHS = 5
         self.BATCH_SIZE = 25
         self.INPUT_SHAPE = [256, 256, 1]
         self.N_IMAGES_TRAIN_VAL = None
-        self.TRAIN_SIZE = 0.8
+        self.TRAIN_SIZE = 0.9
         self.LOSS_NAME = 'cross_entropy_weighted'
-        self.MODEL_NAME = 'flowers_weightedxentropy_lr_005_start_e8_e13_e5_e5.h5'
+        self.MODEL_NAME = 'flowers_weightedxentropy_lr_005_again_10e_5e_lr_0005_5e.h5'
         self.TRAIN_FROM_RESTORE_PATH = '/imatge/pvidal/2018-dlai-team9-joan/models/' \
-                                       'flowers_weightedxentropy_lr_005_start_e8_e13_e5_weights.h5'
+                                       'flowers_weightedxentropy_lr_005_again_10e_5e_weights.h5'
 
         # Dataset file_paths and prior_probs
         self.prior_probs = np.load('data/prior_probs.npy')
@@ -71,9 +71,14 @@ class Trainer(object):
         listdir_train, listdir_val = data.split_train_val(dataset_filepath=self.dataset_filepath,
                                                           train_size=self.TRAIN_SIZE,
                                                           num_images=self.N_IMAGES_TRAIN_VAL)
-        self.generator_train = data.data_generator(listdir=listdir_train, input_shape=self.INPUT_SHAPE,
+        print('Train data size = {0}'.format(len(listdir_train)))
+        print('Validation data size = {0}'.format(len(listdir_val)))
+
+        self.generator_train = data.data_generator(listdir=listdir_train,
+                                                   input_shape=self.INPUT_SHAPE,
                                                    batch_size=self.BATCH_SIZE)
-        self.generator_val = data.data_generator(listdir=listdir_val, input_shape=self.INPUT_SHAPE,
+        self.generator_val = data.data_generator(listdir=listdir_val,
+                                                 input_shape=self.INPUT_SHAPE,
                                                  batch_size=self.BATCH_SIZE)
         self.steps_per_epoch = len(listdir_train) / self.BATCH_SIZE
         self.steps_per_val = len(listdir_val) / self.BATCH_SIZE
